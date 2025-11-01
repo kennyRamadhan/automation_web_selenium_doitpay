c6a66c634074ee2125ec806e1403e3dfed0aeaaf
Kenny Ramadhan <VM915021@maybankcoid.onmicrosoft.com>
2025-11-01 20:13:31 +0700

    Update ChromeDriverConfig

--- src/main/java/com/kenny/doitpay/automation/Config/ChromeDriverFactory.java
+++ src/main/java/com/kenny/doitpay/automation/Config/ChromeDriverFactory.java
@@ -1,22 +1,20 @@
 package com.kenny.doitpay.automation.Config;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-
-import java.util.HashMap;
-import java.util.Map;
-import java.util.List;
-
+import io.github.bonigarcia.wdm.WebDriverManager;
 public class ChromeDriverFactory implements DriverFactory {
 
     @Override
     public WebDriver createDriver() {
-        // Setup driver versi kompatibel otomatis
-        WebDriverManager.chromedriver().setup();
-
-        // Disable logs
+    	
+    	WebDriverManager.chromedriver().setup();
+    	
+        // Nonaktifkan logging yang tidak perlu
         System.setProperty("webdriver.chrome.silentOutput", "true");
         System.setProperty("org.slf4j.simpleLogger.defaultLogLevel", "off");
 
@@ -28,18 +26,26 @@ public class ChromeDriverFactory implements DriverFactory {
         options.addArguments("--disable-popup-blocking");
         options.addArguments("--disable-save-password-bubble");
         options.addArguments("--incognito");
-        options.setExperimentalOption("excludeSwitches", List.of("enable-logging"));
 
+        // Headless mode jika dijalankan di Jenkins
         String browserMode = System.getProperty("BROWSER_MODE", "normal");
         if ("headless".equalsIgnoreCase(browserMode)) {
-            options.addArguments("--headless=new");
+        	
+            options.addArguments("--headless=new"); // new headless di Chrome >109
+            
             options.addArguments("--window-size=1920,1080");
+            options.addArguments("--disable-gpu");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
         }
 
+        // Disable password manager & popups
         Map<String, Object> prefs = new HashMap<>();
         prefs.put("credentials_enable_service", false);
         prefs.put("profile.password_manager_enabled", false);
         options.setExperimentalOption("prefs", prefs);
+
+     
 
         return new ChromeDriver(options);
     }
