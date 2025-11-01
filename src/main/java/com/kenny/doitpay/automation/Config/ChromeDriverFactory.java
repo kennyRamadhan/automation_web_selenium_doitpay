ed81eb1cd62ee4116053f0839e4bf0708b3335de
Kenny Ramadhan <VM915021@maybankcoid.onmicrosoft.com>
2025-11-01 19:59:03 +0700

    Update ChromeDriverConfig

--- src/main/java/com/kenny/doitpay/automation/Config/ChromeDriverFactory.java
+++ src/main/java/com/kenny/doitpay/automation/Config/ChromeDriverFactory.java
@@ -1,28 +1,25 @@
 package com.kenny.doitpay.automation.Config;
 
-import java.util.HashMap;
-import java.util.Map;
-
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-public class ChromeDriverFactory implements DriverFactory {
+import java.util.HashMap;
+import java.util.Map;
+import java.util.List;
 
-    
+public class ChromeDriverFactory implements DriverFactory {
 
     @Override
     public WebDriver createDriver() {
-        
-        // Nonaktifkan logging Netty dan Selenium WebSocket warnings
+        // Setup driver versi kompatibel otomatis
+        WebDriverManager.chromedriver().setup();
+
+        // Disable logs
         System.setProperty("webdriver.chrome.silentOutput", "true");
         System.setProperty("org.slf4j.simpleLogger.defaultLogLevel", "off");
-        System.setProperty("io.netty.noUnsafe", "true");
-        System.setProperty("io.netty.noKeySetOptimization", "true");
-        System.setProperty("io.netty.leakDetection.level", "disabled");
-        System.setProperty("io.netty.eventLoopThreads", "1");
-        
-       
+
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--start-maximized");
         options.addArguments("--disable-notifications");
@@ -31,14 +28,13 @@ public class ChromeDriverFactory implements DriverFactory {
         options.addArguments("--disable-popup-blocking");
         options.addArguments("--disable-save-password-bubble");
         options.addArguments("--incognito");
-        
-        
+        options.setExperimentalOption("excludeSwitches", List.of("enable-logging"));
+
         String browserMode = System.getProperty("BROWSER_MODE", "normal");
         if ("headless".equalsIgnoreCase(browserMode)) {
             options.addArguments("--headless=new");
             options.addArguments("--window-size=1920,1080");
         }
-
 
         Map<String, Object> prefs = new HashMap<>();
         prefs.put("credentials_enable_service", false);
